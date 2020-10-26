697c426b0ae06a448af75f5fbf5527dfa36e73a3
George <sipovsky2010@yandex.ru>
2020-10-27 02:24:37 +0300

    new db scheme, still needs development

--- src/main/resources/db/migration/V1__Init_DB.sql
+++ src/main/resources/db/migration/V1__Init_DB.sql
@@ -1,127 +1,45 @@
-CREATE DATABASE stoom
-    WITH
-    OWNER = postgres
-    TABLESPACE = pg_default
-    CONNECTION LIMIT = -1;
-
-CREATE TABLE public.game
+CREATE TABLE message
 (
-   id character(36) COLLATE pg_catalog."default" NOT NULL,
-   name character(50) COLLATE pg_catalog."default",
-   review_fk character(36) COLLATE pg_catalog."default",
-   url text COLLATE pg_catalog."default",
-   price integer,
-   CONSTRAINT game_pkey PRIMARY KEY (id)
+id VARCHAR (36) PRIMARY KEY,
+text_m VARCHAR (1024),
+time_date TIMESTAMP,
+user_reciever_fk VARCHAR (36),
+user_sender_fk VARCHAR (36),
+FOREIGN KEY (user_sender_fk) REFERENCES user (id)
+);
 
-)
-
-TABLESPACE pg_default;
-
-ALTER TABLE public.game
-   OWNER to postgres;
-
-CREATE TABLE public.message
+CREATE TABLE user
 (
-    id character(36) COLLATE pg_catalog."default" NOT NULL,
-    user_reciever_fk character(36) COLLATE pg_catalog."default",
-    text_m text COLLATE pg_catalog."default",
-    time_date timestamp without time zone,
-    user_sender_fk character(36) COLLATE pg_catalog."default",
-    CONSTRAINT message_pkey PRIMARY KEY (id),
-    )
+id VARCHAR (36) PRIMARY KEY,
+name VARCHAR (128),
+role VARCHAR (32),
+FOREIGN KEY (id) REFERENCES review (user_fk)
+);
 
-TABLESPACE pg_default;
-
-ALTER TABLE public.message
-    OWNER to postgres;
-
-CREATE TABLE public.review
+CREATE TABLE game_user
 (
-    text_review text COLLATE pg_catalog."default",
-    asessment integer,
-    id character(36) COLLATE pg_catalog."default" NOT NULL,
-    user_fk character(36) COLLATE pg_catalog."default",
-    game_fk character(36) COLLATE pg_catalog."default",
-    CONSTRAINT review_pkey PRIMARY KEY (id),
-)
+game_fk VARCHAR (36) PRIMARY KEY,
+user_fk VARCHAR (36),
+FOREIGN KEY (game_fk) REFERENCES game (id),
+FOREIGN KEY (user_fk) REFERENCES user (id)
+);
 
-TABLESPACE pg_default;
-
-ALTER TABLE public.review
-    OWNER to postgres;
-
-CREATE TABLE public.user
+CREATE TABLE game
 (
-    id character(36) COLLATE pg_catalog."default" NOT NULL,
-    name character(20) COLLATE pg_catalog."default",
-    role character(10) COLLATE pg_catalog."default",
-    review_fk character(36) COLLATE pg_catalog."default",
-    CONSTRAINT user_pkey PRIMARY KEY (id),
-    )
+id VARCHAR (36) PRIMARY KEY,
+name VARCHAR (128),
+url VARCHAR (256),
+price INT,
+FOREIGN KEY (id) REFERENCES review (game_fk)
+);
 
-TABLESPACE pg_default;
-
-ALTER TABLE public.user
-    OWNER to postgres;
-
-ALTER TABLE public.game
-    ADD CONSTRAINT g_r FOREIGN KEY (review_fk)
-        REFERENCES public.review (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        NOT VALID;
-
-ALTER TABLE public.message
-    ADD CONSTRAINT m_u_r FOREIGN KEY (user_reciever_fk)
-        REFERENCES public.user (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        NOT VALID;
-
-   ALTER TABLE public.message
-    ADD CONSTRAINT m_u_s FOREIGN KEY (user_sender_fk)
-        REFERENCES public.user (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        NOT VALID;
-
-ALTER TABLE public.review
-    ADD CONSTRAINT r_g FOREIGN KEY (game_fk)
-        REFERENCES public.game (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        NOT VALID;
-
-ALTER TABLE public.review
-    ADD CONSTRAINT r_u FOREIGN KEY (user_fk)
-        REFERENCES public.user (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        NOT VALID;
-
-ALTER TABLE public.user
-    ADD CONSTRAINT u_r FOREIGN KEY (review_fk)
-        REFERENCES public.review (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        NOT VALID;
-
-CREATE TABLE public.game_user
+CREATE TABLE review
 (
-    game_fk character(36) COLLATE pg_catalog."default" NOT NULL,
-    user_fk character(36) COLLATE pg_catalog."default" NOT NULL,
-    CONSTRAINT game_user_pkey PRIMARY KEY (game_fk, user_fk),
-    CONSTRAINT g_u_g FOREIGN KEY (game_fk)
-        REFERENCES public.game (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION,
-    CONSTRAINT g_u_u FOREIGN KEY (user_fk)
-        REFERENCES public.user (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-)
-
-TABLESPACE pg_default;
-
-ALTER TABLE public.game_user
-    OWNER to postgres;
+id VARCHAR (36) PRIMARY KEY,
+text_review VARCHAR (1024),
+assessment REAL,
+user_fk VARCHAR (36),
+game_fk VARCHAR (36),
+FOREIGN KEY (user_fk) REFERENCES user (id),
+FOREIGN KEY (game_fk) REFERENCES game (id)
+);
