75e6342246ec57b24a3a37b0de56589cfe185daa
George <sipovsky2010@yandex.ru>
2020-12-24 23:45:44 +0300

    extended lifetime

--- src/main/java/com/stoom/demo/configuration/AppProperties.java
+++ src/main/java/com/stoom/demo/configuration/AppProperties.java
@@ -22,7 +22,7 @@ public class AppProperties {
     @Setter
     public static class AccessToken implements Token {
         private final String ACCESS_SECRET = "stoomAPIaccesstoken";
-        private final long ACCESS_EXPIRE = 3600000;
+        private final long ACCESS_EXPIRE = 36000000;
 
         private String secret = ACCESS_SECRET;
         private Long expire = ACCESS_EXPIRE;
@@ -32,7 +32,7 @@ public class AppProperties {
     @Getter
     public static class RefreshToken implements Token {
         private final String REFRESH_SECRET = "stoomAPIrefreshtoken";
-        private final long REFRESH_EXPIRE = 86400000;
+        private final long REFRESH_EXPIRE = 864000000;
 
         private String secret = REFRESH_SECRET;
         private Long expire = REFRESH_EXPIRE;
